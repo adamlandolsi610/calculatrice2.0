ae13ccc4efd2d5654f2531fb59687f8c5ee83964
adamlandolsi610 <54667162+adamlandolsi610@users.noreply.github.com>
2019-09-02 15:27:06 +0100

    Add files via upload

---
+++ Calculatrice.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Calculatrice : MonoBehaviour
+{
+    public int A, B;
+    int C;
+    void Start()
+    {
+        float C;
+        C = A + B;
+        Debug.Log("the result is ="+ C + " This is Jam3");
+        C = A - B;
+        Debug.Log("the result is =" + C + " This is TAR7");
+        C = A * B;
+        Debug.Log("the result is =" + C + " This is DHarb");
+        C = A / B;
+        Debug.Log("the result is =" + C + " This is 9ISMA");
+        C = A % B;
+        Debug.Log("the result is =" + C + " This is Be9i el9ISMA");
+
+
+
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
